8119ffa0601905109a0f7945b71f6e13e8ce4ef0
Túlio Trombini <84926327+tuliokenaway74@users.noreply.github.com>
2022-06-30 21:02:04 -0300

    Update familia.pl

--- familia.pl
+++ familia.pl
@@ -1,3 +1,7 @@
+/*==============================================================================================*/
+ 
+/* Relação de pai */
+
 pai(homer, bart).
 pai(homer, lisa).
 pai(homer, maggie).
@@ -6,6 +10,10 @@ pai(ab, herb).
 pai(clancy, marge).
 pai(clancy, patty).
 pai(clancy, selma).
+
+/*==============================================================================================*/
+
+/* Relação de mãe */
 
 mae(marge, bart).
 mae(marge, lisa).
@@ -16,34 +24,69 @@ mae(jackie, patty).
 mae(jackie, selma).
 mae(selma, ling).
 
+/*==============================================================================================*/
+
+/* Relação de descendência */
+
 antecessor(X, Y) :- pai(X, Y).
 antecessor(X, Y) :- mae(X, Y).
 antecessor(X, Y) :- pai(X, Z), antecessor(Z, Y).
 antecessor(X, Y) :- mae(X, Z), antecessor(Z, Y).
 
+/*==============================================================================================*/
+
+/* Relação de avô */
+
 avo_paterno(X, Y) :- pai(X, Z), pai(Z, Y).
 avo_materno(X, Y) :- pai(X, Z), mae(Z, Y).
+
+/*==============================================================================================*/
+
+/* Relação de avó */
 
 avoh_paterna(X, Y) :- mae(X, Z), pai(Z, Y).
 avoh_materna(X, Y) :- mae(X, Z), mae(Z, Y).
 
+/*==============================================================================================*/
+
+/* Relação de irmão */
+
 irmao(X, Y) :- pai(Z, X), pai(Z, Y), X \= Y.
-irmao(X, Y) :- mae(Z, X), mae(Z, Y), X \= Y.
+
+/*==============================================================================================*/
+
+/* tem filho? */
 
 tem_filho(X) :- pai(X, _).
 tem_filho(X) :- mae(X, _).
 
+/*==============================================================================================*/
+
+/* tem mesmo filho? */
+
 tem_mesmo_filho(X, Y) :- pai(X, Z), mae(Y, Z).
 
-tio(X, Y) :- irmao(X, Z), pai(Z, Y).
-tio(X, Y) :- irmao(X, Z), mae(Z, Y).
+/*==============================================================================================*/
 
-primo(X, Y) :- pai(Z, X), pai(W, Y), irmao(Z, W).
-primo(X, Y) :- pai(Z, X), mae(W, Y), irmao(Z, W).
-primo(X, Y) :- mae(Z, X), mae(W, Y), irmao(Z, W).
-primo(X, Y) :- mae(Z, X), pai(W, Y), irmao(Z, W).
+/* Relação de irmão */
+
+tio(X, Y) :- irmao(Z, X), pai(Z, Y), X \= Z.
+tio(X, Y) :- irmao(Z, X), mae(Z, Y), X \= Z.
+
+/*==============================================================================================*/
+
+/* Relação de primo */
+
+primo(X, Y) :- tio(Z, X), pai(Z, Y).
+primo(X, Y) :- tio(Z, X), mae(Z, Y).
+
+/*==============================================================================================*/
+
+/* Relação de neto */
 
 neto(X, Y) :- pai(Y, Z), pai(Z, X).
 neto(X, Y) :- mae(Y, Z), pai(Z, X).
 neto(X, Y) :- pai(Y, Z), mae(Z, X).
 neto(X, Y) :- mae(Y, Z), mae(Z, X).
+
+/*==============================================================================================*/
